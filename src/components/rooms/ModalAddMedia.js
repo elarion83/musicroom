e79fc91d47b4569acfda003a9d6cae3108f4418b
nicolas gruwe <gruwe.nicolas@hotmail.fr>
2023-06-01 02:22:10 +0200

    add type writer field effect

--- src/components/rooms/ModalAddMedia.js
+++ src/components/rooms/ModalAddMedia.js
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Alert from '@mui/material/Alert';
 
+import Container from '@mui/material/Container';
 import Dialog from '@mui/material/Dialog';
 import { v4 as uuid } from 'uuid';
 import InputAdornment from '@mui/material/InputAdornment';
 import AddIcon from '@mui/icons-material/Add';
+import Typed from "react-typed"
 
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import ListSubheader from '@mui/material/ListSubheader';
+import Snackbar, { SnackbarOrigin } from '@mui/material/Snackbar';
 import ListItemText from '@mui/material/ListItemText';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -23,17 +26,18 @@ import YTSearch from 'youtube-api-search';
 import SearchIcon from '@mui/icons-material/Search';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import RefreshIcon from '@mui/icons-material/Refresh';
+import InputBase from '@mui/material/InputBase';
 
+const RoomModalAddMedia = ({ validatedObjectToAdd }) => {
 
-const RoomModalAddMedia = ({ validatedObjectToAdd, isOpen }) => {
-
-    const [OpenAddToPlaylistModal, setOpenAddToPlaylistModal] = useState(isOpen);
     const [mediaSearchResultYoutube, setMediaSearchResultYoutube] = useState([]);
     const [mediaSearchResultDailyMotion, setMediaSearchResultDailyMotion] = useState([]);
     const [addingUrl, setAddingUrl] = useState('');
     const [searchTerm, setSearchTerm] = useState('');
     const [recentlyAdded, setRecentlyAdded]= useState(false);
+    const [isSearching, setIsSearching]= useState(false);
     const delay = ms => new Promise(res => setTimeout(res, ms));
     
     const [addingObject, setAddingObject] = useState({title:'',source:'',url:'', addedBy: localStorage.getItem("MusicRoom_UserInfoPseudo")})
@@ -49,13 +53,13 @@ const RoomModalAddMedia = ({ validatedObjectToAdd, isOpen }) => {
         addingObject.title='';
         addingObject.source='';
         addingObject.url='';
+        setRecentlyAdded(false);
         setRecentlyAdded(true);
         await delay(2000);
         setRecentlyAdded(false);
     }
         
     async function getYoutubeVideoInfosFromId(videoId, addingObject) {
-
                     var params = {
                         part: 'snippet',
                         key: 'AIzaSyAcFecOONJZvjwMnTB9Fv9x753KWsVUvWM',
@@ -71,6 +75,7 @@ const RoomModalAddMedia = ({ validatedObjectToAdd, isOpen }) => {
     }
 
     async function handleSearchForMedia() {
+        setIsSearching(true);
         if(searchTerm !== '') {
             if (validator.isURL(searchTerm.trim())) {
                 addingObject.url = searchTerm.trim();
@@ -93,58 +98,67 @@ const RoomModalAddMedia = ({ validatedObjectToAdd, isOpen }) => {
                 addingObject.hashId = uuid().slice(0,10).toLowerCase()
                 handleCheckAndAddObjectToPlaylistFromObject(addingObject);
                 setSearchTerm('');
+                setIsSearching(false);
             } else {
                 YTSearch({key: 'AIzaSyAcFecOONJZvjwMnTB9Fv9x753KWsVUvWM', term: searchTerm}, (videos) => {
                     setMediaSearchResultYoutube(videos);
-                    console.log(videos);
                 });
 
                 fetch('https://api.dailymotion.com/videos?fields=id,thumbnail_url%2Ctitle&country=fr&search='+searchTerm+'&limit=5')
                     .then((response) => response.json())
                     .then((responseJson) => {
-                        console.log(responseJson.list);
                         setMediaSearchResultDailyMotion(responseJson.list);
+                        setIsSearching(false);
                 })
             }
         }
-        
     }
 
     return(
-        
-        <Dialog fullScreen open={isOpen} onClose={(e) => setOpenAddToPlaylistModal(false)} >
-                <DialogTitle sx={{pb:0}}>Ajouter dans la playlist</DialogTitle>
-                  <Grid item xs={12}>
-                    <TextField
-                        id="addMediaSearchInput"
-                        type="text"
-                        onKeyPress={(ev) => {
-                        if (ev.key === 'Enter')  { handleSearchForMedia()}
-                        }}
-                        label="Votre recherche "
-                        helperText="https://www.youtube.com/watch?v=vslZZLpQZz0, Vald, Lomepal, Rammstein, Dua Lipa, .."
-                        value={searchTerm}
-                        onChange={e => setSearchTerm(e.target.value)}
-                        sx={{ width: '100%', paddingRight:'0', "& .MuiOutlinedInput-root": {
-                            paddingRight: 0,
-                            wordBreak: 'break-all'
-                        } }}
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment  sx={{
-                                padding: "27.5px 14px",
-                                backgroundColor: "#23282d",
-                                color:'white',
-                                cursor:'pointer',
-                                }} position="end" onClick={handleSearchForMedia}>
-                                <SearchIcon  />
-                                </InputAdornment>
-                            ),
-                        }}
-                    />
+            <Container sx={{padding:'3em',pt:0, height:'100vh', zIndex:3}} className="full_width_modal_content_container">
+                  <Grid item xs={12} sx={{ mt:2, display:'flex', flexDirection:'row'}} className="autowriter_container">
+                     <Typed
+                        strings={[
+                            'UNE MUSIQUE',
+                            'UNE VIDEO',
+                            'UN ALBUM',
+                            'UN LIEN SOUNCLOUND',
+                            'UN LIEN YOUTUBE',
+                            'DAFT PUNK - ALIVE (LIVE)',
+                            'MKRB - DOM P',
+                            'GAZO - LA RUE',
+                            'PNL - AU DD',
+                            'DOMINGO - REACT',
+                            'HTTPS://WWW.YOUTUBE.COM/WATCH?V=MAVIDEO']}
+                        typeSpeed={120}
+                        showCursor={true}
+                        backSpeed={20}
+                        attr="placeholder"
+                    loop >
+                        <input 
+                            id="addMediaSearchInput" 
+                            className="input_big typed"
+                            type='text' 
+                            variant="standard" 
+                            onKeyPress={(ev) => {
+                            if (ev.key === 'Enter')  { handleSearchForMedia()}
+                            }}                         
+                            onChange={e => setSearchTerm(e.target.value)}
+                            value={searchTerm} 
+                            style={{ width: '100%', height:'100%', flexGrow:1,paddingRight:'0', bgcolor:'rgba(255, 255, 255, 0.1)', pl:2}}
+                            />
+                    </Typed> 
+                    <Button  sx={{
+                        padding: "27.5px 14px",
+                        backgroundColor: "#131416",
+                        color:'white',
+                        cursor:'pointer',
+                        }} position="end" onClick={handleSearchForMedia}>
+                        {!isSearching ? <SearchIcon  /> : <RefreshIcon  />}
+                    </Button>
                   </Grid>
                   {mediaSearchResultYoutube.length > 1 && <Grid item xs={12}>
-                    <Tabs value={tabIndex} onChange={handleTabChange}>
+                    <Tabs value={tabIndex} onChange={handleTabChange} sx={{bgcolor:'#202124'}}>
                         {mediaSearchResultYoutube.length > 1  && <Tab label="Youtube" />}
                         {mediaSearchResultDailyMotion.length > 1 && <Tab label="Dailymotion" />}
                     </Tabs>
@@ -154,10 +168,10 @@ const RoomModalAddMedia = ({ validatedObjectToAdd, isOpen }) => {
                             {mediaSearchResultYoutube.length > 1 && <Grid item xs={12}>
                                 <List component="nav">
                                     { mediaSearchResultYoutube.map(function(media, idx){
-                                        return (<ListItemButton sx={{margin:0,mb:1, padding:0, pr:1}}  key={idx} onClick={(e) => handleCheckAndAddObjectToPlaylistFromObject({title:media.snippet.title, source:'youtube', url:'https://www.youtube.com/watch?v='+media.id.videoId, addedBy: addingObject.addedBy, vote: {'up':0,'down':0}, hashId: uuid().slice(0,10).toLowerCase()})}>
+                                        return (<ListItemButton sx={{margin:0, padding:0, pr:1,borderBottom: '2px solid #3e464d'}}  key={idx} onClick={(e) => handleCheckAndAddObjectToPlaylistFromObject({title:media.snippet.title, source:'youtube', url:'https://www.youtube.com/watch?v='+media.id.videoId, addedBy: addingObject.addedBy, vote: {'up':0,'down':0}, hashId: uuid().slice(0,10).toLowerCase()})}>
                                             <img src={media.snippet.thumbnails.default.url} />
                                             <Grid sx={{display:'flex',flexDirection:'column',pl:2}}>
-                                                <ListItemText primary={media.snippet.title.substring(0, 50)} sx={{ mt:0, fontSize:'0.9em'}}/>
+                                                <ListItemText primary={media.snippet.title.substring(0, 50)} className='video_title_list' sx={{ mt:0, fontSize:'0.9em'}}/>
                                                 <Typography sx={{ ml:0, mb: 0, fontSize: '10px', textTransform:'uppercase' }}>Publié par <b>{media.snippet.channelTitle}</b></Typography>
                                             </Grid>
                                             </ListItemButton>)
@@ -171,7 +185,7 @@ const RoomModalAddMedia = ({ validatedObjectToAdd, isOpen }) => {
                             {mediaSearchResultDailyMotion.length > 1 && <Grid item xs={12}>
                             <List component="nav">
                                 { mediaSearchResultDailyMotion.map(function(media, idx){
-                                    return (<ListItemButton sx={{margin:0,mb:1, padding:0, pr:1}} key={idx} onClick={(e) => handleCheckAndAddObjectToPlaylistFromObject({title:media.title, source:'dailymotion', url:'https://www.dailymotion.com/video/'+media.id, addedBy: addingObject.addedBy, vote: {'up':0,'down':0}, hashId: uuid().slice(0,10).toLowerCase()})}>
+                                    return (<ListItemButton sx={{margin:0,padding:0, pr:1,borderBottom: '2px solid #3e464d'}} key={idx} onClick={(e) => handleCheckAndAddObjectToPlaylistFromObject({title:media.title, source:'dailymotion', url:'https://www.dailymotion.com/video/'+media.id, addedBy: addingObject.addedBy, vote: {'up':0,'down':0}, hashId: uuid().slice(0,10).toLowerCase()})}>
                                         <img src={media.thumbnail_url} style={{width:'120px', height:'90px'}}/>
                                         <ListItemText primary={media.title} sx={{ml:2, mt:0, fontSize:'0.9em'}}/></ListItemButton>)
                                 }) }
@@ -181,8 +195,13 @@ const RoomModalAddMedia = ({ validatedObjectToAdd, isOpen }) => {
                         )}
                     </Box>
                   </Grid>}
-                  {recentlyAdded && <Alert severity="success" sx={{margin:2}}> Bien ajouté à la playlist !</Alert>}
-        </Dialog>
+                  <Snackbar
+                    open={recentlyAdded}
+                    autoHideDuration={6000}
+                    sx={{bgcolor:'#2e7d32', borderRadius:'2px'}}
+                    message="Bien ajouté à la playlist !"
+                    />
+    </Container>
     )
 };
 
