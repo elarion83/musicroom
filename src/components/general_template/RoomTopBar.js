ff094a275b9040ef217debd9d0e038aaec5d5cdb
nicolas gruwe <gruwe.nicolas@hotmail.fr>
2023-06-01 18:21:00 +0200

    modal before leaving a room

--- src/components/general_template/RoomTopBar.js
+++ src/components/general_template/RoomTopBar.js
@@ -13,9 +13,18 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import InfoIcon from '@mui/icons-material/Info';
 import ListItemIcon from '@mui/material/ListItemIcon';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import Button from '@mui/material/Button';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const RoomTopBar = ({roomId, handleOpenShareModal, roomAdmin }) => {
     
+    
+    const [openLeaveRoomModal, setOpenLeaveRoomModal] = useState(false);
+
     function handleClickMenu(event) {
         setAnchorEl(event.currentTarget);
     };
@@ -27,7 +36,6 @@ const RoomTopBar = ({roomId, handleOpenShareModal, roomAdmin }) => {
     const [anchorEl, setAnchorEl] = useState(null);
     
     function handleQuitRoom() {
-
         if(localStorage.getItem("MusicRoom_RoomId")) {
             localStorage.removeItem("MusicRoom_RoomId");
             window.location.reload(false);
@@ -36,72 +44,92 @@ const RoomTopBar = ({roomId, handleOpenShareModal, roomAdmin }) => {
     }
   return (
     <AppBar position="sticky" className='sticky_top' >
-            <Toolbar xs={12}sx={{ bgcolor: '#262626',borderBottom: '2px solid #3e464d', minHeight: '45px !important', fontFamily: 'Monospace', pl:'5px', pr:'25 px' }}>
+        <Toolbar xs={12}sx={{ bgcolor: '#262626',borderBottom: '2px solid #3e464d', minHeight: '45px !important', fontFamily: 'Monospace', pl:'5px', pr:'25 px' }}>
                 
-                    <Tooltip title="Paramètres de la room" sx={{ bgColor:'#30363c'}}>
-                        <IconButton
-                            size="small"
-                            aria-haspopup="true"
-                            onClick={e => handleClickMenu(e)}
-                            color="inherit"
-                            sx={{bgcolor:'#202124', mr:1}}
-                        >
-                            <TuneIcon />
-                        </IconButton>
-                    </Tooltip>
-                    <Menu
-                        id="menu-appbar"
-                        anchorEl={anchorEl}
-                        anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                        }}
-                        
-                        onClose={handleClose}
-                        onClick={handleClose}
-                        open={Boolean(anchorEl)}
+                <Tooltip title="Paramètres de la room" sx={{ bgColor:'#30363c'}}>
+                    <IconButton
+                        size="small"
+                        aria-haspopup="true"
+                        onClick={e => handleClickMenu(e)}
+                        color="inherit"
+                        sx={{bgcolor:'#202124', mr:1}}
                     >
-                        <MenuItem>
-                            <ListItemIcon>
-                                <InfoIcon  fontSize="small" />
-                            </ListItemIcon>
-                            <Typography>
-                            Hosté par <b>{roomAdmin}</b>
-                            </Typography>
-                        </MenuItem>
-                        <Divider />
-                        <MenuItem >
-                            <ListItemIcon>
-                                <TuneIcon fontSize="small" />
-                            </ListItemIcon>
-                            <Typography>
-                            Paramètres 
-                            </Typography>
-                        </MenuItem>
-                        <MenuItem onClick={e => handleOpenShareModal(true)}>
-                            <ListItemIcon>
-                                <ShareIcon fontSize="small" />
-                            </ListItemIcon>
-                            <Typography>
-                            Partager la room
-                            </Typography>
-                        </MenuItem>
-                        <MenuItem onClick={e => handleQuitRoom()}>
-                            <ListItemIcon>
-                                <ExitToAppIcon fontSize="small" />
-                            </ListItemIcon>
-                            <Typography>
-                            Quitter la room
-                            </Typography>
-                        </MenuItem>
-                    </Menu>
+                        <TuneIcon />
+                    </IconButton>
+                </Tooltip>
+                <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                    }}
+                    
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    open={Boolean(anchorEl)}
+                >
+                    <MenuItem>
+                        <ListItemIcon>
+                            <InfoIcon  fontSize="small" />
+                        </ListItemIcon>
+                        <Typography>
+                        Hosté par <b>{roomAdmin}</b>
+                        </Typography>
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem >
+                        <ListItemIcon>
+                            <TuneIcon fontSize="small" />
+                        </ListItemIcon>
+                        <Typography>
+                        Paramètres 
+                        </Typography>
+                    </MenuItem>
+                    <MenuItem onClick={e => handleOpenShareModal(true)}>
+                        <ListItemIcon>
+                            <ShareIcon fontSize="small" />
+                        </ListItemIcon>
+                        <Typography>
+                        Partager la room
+                        </Typography>
+                    </MenuItem>
+                    <MenuItem onClick={e => setOpenLeaveRoomModal(true)}>
+                        <ListItemIcon>
+                            <ExitToAppIcon fontSize="small" />
+                        </ListItemIcon>
+                        <Typography>
+                        Quitter la room
+                        </Typography>
+                    </MenuItem>
+                </Menu>
 
-                <Typography  component="div" sx={{ flexGrow: 1 , textTransform:'uppercase', fontSize:'12px',}}>
-                    Room : <b>{ roomId } </b> 
-                </Typography>
-               
-            </Toolbar>
-        </AppBar>
+            <Typography  component="div" sx={{ flexGrow: 1 , textTransform:'uppercase', fontSize:'12px',}}>
+                Room : <b><span>{ roomId }</span> </b> 
+            </Typography>
+            
+        </Toolbar>
+        
+        <Dialog open={openLeaveRoomModal} keepMounted onClose={(e) => setOpenLeaveRoomModal(false)} >
+            
+            <DialogTitle id="alert-dialog-title">
+                Quitter la room ?
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText >
+                Vous êtes sur le point de quitter la room pour retourner à l'accueil.
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button variant="outlined" onClick={(e) => handleQuitRoom(false)}>
+                    Quitter
+                </Button>
+                <Button variant="outlined" onClick={(e) => setOpenLeaveRoomModal(false)} autoFocus>
+                    Rester
+                </Button>
+            </DialogActions>
+        </Dialog>
+    </AppBar>
   )
 };
 
