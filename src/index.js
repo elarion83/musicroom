1b4f1988c2544d6c791ad4a323e5b0b105597015
nicolas gruwe <gruwe.nicolas@hotmail.fr>
2023-06-16 23:04:59 +0200

    previous fix for console error is wrong lol

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "@fontsource/roboto";
 import '@fontsource/roboto/100.css';
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
