87808703933eec77439424a24e9db1b184bd1082
nicolas gruwe <gruwe.nicolas@hotmail.fr>
2023-06-27 20:06:58 +0200

    privacy policy and youtube api compliance

--- src/components/generalsTemplates/modals/LoginModal.js
+++ src/components/generalsTemplates/modals/LoginModal.js
@@ -1,15 +1,17 @@
 import { Icon } from "@iconify/react";
-import { Alert, Box, Button, Dialog, DialogContent, DialogContentText, DialogTitle, Divider, Grid } from "@mui/material";
+import { Alert, Box, Button, Dialog, DialogContent, DialogContentText, DialogTitle, Divider, Grid, Typography } from "@mui/material";
 import { useState } from "react";
 
 import TextField from '@mui/material/TextField';
 
 import { LoadingButton } from "@mui/lab";
+import PrivacyPolicyModal from '../modals/PrivacyPolicyModal';
 
 const LoginModal = ({ open, changeOpen, handleAnonymousLogin, handleGoogleLogin, handlePasswordAndMailLogin, loginLoading, redirectToHome, roomId, loginErrorMessage}) => {
     
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('');
+    const [PrivacyPolicyModalOpen, setPrivacyPolicyModalOpen] = useState(false);
         
     async function onEmailAndPasswordSubmit() {
         handlePasswordAndMailLogin(email, password); 
@@ -98,6 +100,10 @@ const LoginModal = ({ open, changeOpen, handleAnonymousLogin, handleGoogleLogin,
                                 Vers l'accueil
                             </Button> }
                     </Grid>
+
+                    <Typography fontSize="small">En utilisant ce service vous acceptez notre <a href="#" onClick={(e) => setPrivacyPolicyModalOpen(true)}>politique de confidentialité</a> et <a href="https://www.youtube.com/t/terms" target="_blank">les conditions d'utilisation de Youtube.</a>
+                    </Typography>
+                    <PrivacyPolicyModal open={PrivacyPolicyModalOpen} changeOpen={setPrivacyPolicyModalOpen} />
                 </DialogContentText>
             </DialogContent>
         </Dialog>
