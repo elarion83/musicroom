daa2c16d4b1cdf3b9d76a942093124147ef0b16c
nicolas gruwe <gruwe.nicolas@hotmail.fr>
2024-06-03 03:13:27 +0200

    last try for tuto zindex on mobile with small layout

--- src/components/rooms/RoomTutorial.js
+++ src/components/rooms/RoomTutorial.js
@@ -8,14 +8,16 @@ const RoomTutorial = ({layout,slideOutProp}) => {
         <>
         <Box    
         sx={{bottom:slideOutProp}}
-            ref={el => animatedElementsRef.push(el)} 
             className={"roomTutorialContainer "+layout+" phoneAndTablet animate__animated animate__fadeInUp animate__slow"}>
-            <Typography className="fontFamilyNunito" sx={{color:'white', textShadow: '2px 8px 6px rgba(0,0,0,0.2), 0px -5px 35px rgba(255,255,255,0.3)'}}>Swipe pour rechercher ! </Typography>
+            <Typography className="fontFamilyNunito" 
+            sx={{color:'white', 
+            
+            textShadow: '2px 2px 6px rgba(0,0,0,1), 0px -5px 35px rgba(0,0,0,1)'}}
+            >Swipe pour rechercher ! </Typography>
             <SwipeUpIcon className="SlideUp"/>
         </Box>
         <Box 
         sx={{bottom:slideOutProp}}
-            ref={el => animatedElementsRef.push(el)} 
             className="roomTutorialContainer desktop animate__animated animate__fadeInUp animate__slow">
             <Typography className="fontFamilyNunito" sx={{color:'white'}}>Clique pour rechercher ! </Typography>
             <KeyboardDoubleArrowDownIcon className="ClickUp"/>
