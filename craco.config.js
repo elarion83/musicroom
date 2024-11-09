7fa06666cf1b20212d813e5a49d88ac3fab93285
Nico <gruwe.nicolas@hotmail.fr>
2024-11-09 16:16:32 +0700

    fix for build and deploy

--- craco.config.js
+++ craco.config.js
@@ -3,5 +3,13 @@ module.exports = {
     postcss: {
       plugins: [ require("autoprefixer")],
     },
+  },
+  babel: {
+    "plugins": [
+      ["@babel/plugin-proposal-class-properties", {loose: true}],
+      ["@babel/plugin-proposal-private-methods", {loose: true}],
+      ["@babel/plugin-proposal-optional-chaining", {loose: true}],
+      ["@babel/plugin-proposal-nullish-coalescing-operator", {loose: true}],
+    ]
   }
 };
